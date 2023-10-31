4d3b3f94e7168c58a9b52f1e13b8f3b6c68f4df3
Sandip Sah <sandip123456sah@gmail.com>
2023-10-31 10:05:06 +0545

    updated commit

--- Random_Questions/NumberToWord.cpp
+++ Random_Questions/NumberToWord.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
 // Arrays to store words for ones, tens, and teens places
 string ones[] = {"","one","two","three","four","five","six","seven","eight","nine"};
 string teens[] = {"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
-string tens[] = {"","","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
+string tens[] = {"","ten","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
 
 string NumberToWord(int num){
-    if(num < 0 || num > 999){
+    if(num < 0 || num > 999999){
         return "Number out of range.";
     }
     if(num == 0){
@@ -14,7 +15,50 @@ string NumberToWord(int num){
     }
     string result;  // initializing the result string 
     
-    // handling hundred place
+    // handling lakh place 
+    if(num >= 100000 && num <= 999999){
+        result += ones[num/100000] + " lakh ";
+        num %= 100000;
+    }
+    // handling ten thousand place
+    if(num >= 10000 && num < 11000){
+        result += tens[num / 10000] ;
+        num %= 10000;
+        if(num >= 100 && num <= 999){
+            result += " thousand ";
+        }
+        if(num == 0){
+            result += " thousand ";
+        }
+    }
+    if(num >=10000 && num <= 19999){
+        num %= 10000;
+        result += teens[num/1000] + " thousand ";
+        //num ;
+        num %= 1000;
+    }
+    if(num >= 20000 && num <= 99999){
+        result += tens[num/10000];
+        num %= 10000;
+        if(num >= 100 && num <= 999){
+            result += " thousand ";
+        }
+        if(num == 0){
+            result += " thousand ";
+        }
+        if(num >= 0 && num <= 999){
+            result += "";
+        }
+        else{
+            result += "-";
+        }
+    }
+    //handling thousand place
+    if(num >= 1000 && num <= 9999){
+        result += ones[num/1000]+" thausand ";
+        num %= 1000;
+    }
+    //handling hundred place
     if(num >=100 && num <=999){
         result += ones[num/100] + " hundred ";
         num %= 100;
@@ -22,13 +66,13 @@ string NumberToWord(int num){
     // handling tens place
     if(num >= 10 && num <=19){
         result +=  teens[num%10];
-        return result;
+        // return result;
     }
     else if(num >=20 && num <= 99){
         result += tens[num/10];
         num %= 10;
         if(num>0){
-            result += " ";
+            result += "-";
         }
     }
     // handling ones place 
@@ -40,9 +84,11 @@ string NumberToWord(int num){
 }
 int main(){ 
     int number ;
-    cout<<"Enter the number between 1 to 999 : ";
+    cout<<endl;
+    cout<<"Enter the number between 1 to 999999 --> ";
     cin>>number;
     string word = NumberToWord(number);
-    cout<<"The result is : "<<word;
+    cout<<"The result is :-> "<<word<<endl;
+    cout<<endl;
     return 0;
 }
---
+++ Random_Questions/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+if(num >= 10000 && num < 11000){
+    //     result += tens[num / 10000] + " thousand ";
+    //     num %= 10000;
+    // }
